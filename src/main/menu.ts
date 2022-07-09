10352493f6b329a2f36da5c9ed371271f18cfce1
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-07-09 11:23:27 +0900

    ✨: メニュー

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -1,9 +1,5 @@
-import {app, BrowserWindow, Menu, MenuItemConstructorOptions, shell} from 'electron';
-
-interface DarwinMenuItemConstructorOptions extends MenuItemConstructorOptions {
-  selector?: string;
-  submenu?: DarwinMenuItemConstructorOptions[] | Menu;
-}
+import {app, BrowserWindow, dialog, Menu, MenuItemConstructorOptions} from 'electron';
+import {isMac, macOrOther} from './utils/platform';
 
 export default class MenuBuilder {
   mainWindow: BrowserWindow;
@@ -17,7 +13,7 @@ export default class MenuBuilder {
       this.setupDevelopmentEnvironment();
     }
 
-    const template = process.platform === 'darwin' ? this.buildDarwinTemplate() : this.buildDefaultTemplate();
+    const template = this.buildTemplate();
 
     const menu = Menu.buildFromTemplate(template);
     Menu.setApplicationMenu(menu);
@@ -40,157 +36,58 @@ export default class MenuBuilder {
     });
   }
 
-  buildDarwinTemplate(): MenuItemConstructorOptions[] {
-    const subMenuAbout: DarwinMenuItemConstructorOptions = {
-      label: 'Electron',
-      submenu: [
-        {
-          label: 'About ElectronReact',
-          selector: 'orderFrontStandardAboutPanel:',
-        },
-        {type: 'separator'},
-        {label: 'Services', submenu: []},
-        {type: 'separator'},
-        {
-          label: 'Hide ElectronReact',
-          accelerator: 'Command+H',
-          selector: 'hide:',
-        },
-        {
-          label: 'Hide Others',
-          accelerator: 'Command+Shift+H',
-          selector: 'hideOtherApplications:',
-        },
-        {label: 'Show All', selector: 'unhideAllApplications:'},
-        {type: 'separator'},
-        {
-          label: 'Quit',
-          accelerator: 'Command+Q',
-          click: () => {
-            app.quit();
+  buildTemplate(): MenuItemConstructorOptions[] {
+    const macMenus: MenuItemConstructorOptions[] = isMac
+      ? [
+          {
+            label: app.name,
+            submenu: [
+              {role: 'about', label: `${app.name}について`},
+              {type: 'separator'},
+              {role: 'hide', label: `${app.name}を隠す`},
+              {role: 'unhide', label: 'すべて表示'},
+              {type: 'separator'},
+              {role: 'quit', label: `${app.name}を終了`},
+            ],
           },
-        },
-      ],
-    };
-    const subMenuEdit: DarwinMenuItemConstructorOptions = {
-      label: 'Edit',
-      submenu: [
-        {label: 'Undo', accelerator: 'Command+Z', selector: 'undo:'},
-        {label: 'Redo', accelerator: 'Shift+Command+Z', selector: 'redo:'},
-        {type: 'separator'},
-        {label: 'Cut', accelerator: 'Command+X', selector: 'cut:'},
-        {label: 'Copy', accelerator: 'Command+C', selector: 'copy:'},
-        {label: 'Paste', accelerator: 'Command+V', selector: 'paste:'},
-        {
-          label: 'Select All',
-          accelerator: 'Command+A',
-          selector: 'selectAll:',
-        },
-      ],
-    };
-    const subMenuViewDev: MenuItemConstructorOptions = {
-      label: 'View',
-      submenu: [
-        {
-          label: 'Reload',
-          accelerator: 'Command+R',
-          click: () => {
-            this.mainWindow.webContents.reload();
-          },
-        },
-        {
-          label: 'Toggle Full Screen',
-          accelerator: 'Ctrl+Command+F',
-          click: () => {
-            this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
-          },
-        },
-        {
-          label: 'Toggle Developer Tools',
-          accelerator: 'Alt+Command+I',
-          click: () => {
-            this.mainWindow.webContents.toggleDevTools();
-          },
-        },
-      ],
-    };
-    const subMenuViewProd: MenuItemConstructorOptions = {
-      label: 'View',
-      submenu: [
-        {
-          label: 'Toggle Full Screen',
-          accelerator: 'Ctrl+Command+F',
-          click: () => {
-            this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
-          },
-        },
-      ],
-    };
-    const subMenuWindow: DarwinMenuItemConstructorOptions = {
-      label: 'Window',
-      submenu: [
-        {
-          label: 'Minimize',
-          accelerator: 'Command+M',
-          selector: 'performMiniaturize:',
-        },
-        {label: 'Close', accelerator: 'Command+W', selector: 'performClose:'},
-        {type: 'separator'},
-        {label: 'Bring All to Front', selector: 'arrangeInFront:'},
-      ],
-    };
-    const subMenuHelp: MenuItemConstructorOptions = {
-      label: 'Help',
-      submenu: [
-        {
-          label: 'Learn More',
-          click() {
-            shell.openExternal('https://electronjs.org');
-          },
-        },
-        {
-          label: 'Documentation',
-          click() {
-            shell.openExternal('https://github.com/electron/electron/tree/main/docs#readme');
-          },
-        },
-        {
-          label: 'Community Discussions',
-          click() {
-            shell.openExternal('https://www.electronjs.org/community');
-          },
-        },
-        {
-          label: 'Search Issues',
-          click() {
-            shell.openExternal('https://github.com/electron/electron/issues');
-          },
-        },
-      ],
-    };
+        ]
+      : [];
 
-    const subMenuView =
-      process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true' ? subMenuViewDev : subMenuViewProd;
-
-    return [subMenuAbout, subMenuEdit, subMenuView, subMenuWindow, subMenuHelp];
-  }
-
-  buildDefaultTemplate() {
-    const templateDefault = [
+    return [
+      ...macMenus,
       {
-        label: '&File',
+        label: MenuBuilder.label('File'),
         submenu: [
           {
-            label: '&Open',
-            accelerator: 'Ctrl+O',
-          },
-          {
-            label: '&Close',
-            accelerator: 'Ctrl+W',
-            click: () => {
-              this.mainWindow.close();
+            label: MenuBuilder.label('Open'),
+            accelerator: macOrOther({
+              mac: 'Command+O',
+              other: 'Ctrl+O',
+            }),
+            click: async () => {
+              const {canceled} = await dialog.showOpenDialog(this.mainWindow, {
+                filters: [{name: 'JSON Files', extensions: ['json']}],
+              });
+              if (!canceled) {
+                try {
+                  dialog.showErrorBox('成功', 'データの読み込みに成功しました');
+                } catch (e) {
+                  dialog.showErrorBox('エラー', 'データの読み込みに失敗しました');
+                }
+              }
             },
           },
+          ...(isMac
+            ? []
+            : [
+                {
+                  label: MenuBuilder.label('Close'),
+                  accelerator: 'Alt+F4',
+                  click: () => {
+                    this.mainWindow.close();
+                  },
+                },
+              ]),
         ],
       },
       {
@@ -199,22 +96,37 @@ export default class MenuBuilder {
           process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true'
             ? [
                 {
-                  label: '&Reload',
-                  accelerator: 'Ctrl+R',
+                  label: MenuBuilder.label('Reload'),
+                  accelerator: macOrOther({
+                    mac: 'Command+R',
+                    other: 'Ctrl+R',
+                  }),
                   click: () => {
                     this.mainWindow.webContents.reload();
                   },
                 },
                 {
-                  label: 'Toggle &Full Screen',
-                  accelerator: 'F11',
+                  label: macOrOther({
+                    mac: 'Toggle Full Screen',
+                    other: 'Toggle &Full Screen',
+                  }),
+                  accelerator: macOrOther({
+                    mac: 'Ctrl+Command+F',
+                    other: 'F11',
+                  }),
                   click: () => {
                     this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
                   },
                 },
                 {
-                  label: 'Toggle &Developer Tools',
-                  accelerator: 'Alt+Ctrl+I',
+                  label: macOrOther({
+                    mac: 'Toggle Developer Tools',
+                    other: 'Toggle &Developer Tools',
+                  }),
+                  accelerator: macOrOther({
+                    mac: 'Alt+Command+I',
+                    other: 'Alt+Ctrl+I',
+                  }),
                   click: () => {
                     this.mainWindow.webContents.toggleDevTools();
                   },
@@ -222,45 +134,24 @@ export default class MenuBuilder {
               ]
             : [
                 {
-                  label: 'Toggle &Full Screen',
-                  accelerator: 'F11',
+                  label: macOrOther({
+                    mac: 'Toggle Full Screen',
+                    other: 'Toggle &Full Screen',
+                  }),
+                  accelerator: macOrOther({
+                    mac: 'Ctrl+Command+F',
+                    other: 'F11',
+                  }),
                   click: () => {
                     this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
                   },
                 },
               ],
       },
-      {
-        label: 'Help',
-        submenu: [
-          {
-            label: 'Learn More',
-            click() {
-              shell.openExternal('https://electronjs.org');
-            },
-          },
-          {
-            label: 'Documentation',
-            click() {
-              shell.openExternal('https://github.com/electron/electron/tree/main/docs#readme');
-            },
-          },
-          {
-            label: 'Community Discussions',
-            click() {
-              shell.openExternal('https://www.electronjs.org/community');
-            },
-          },
-          {
-            label: 'Search Issues',
-            click() {
-              shell.openExternal('https://github.com/electron/electron/issues');
-            },
-          },
-        ],
-      },
     ];
+  }
 
-    return templateDefault;
+  private static label(labelName: string): string {
+    return isMac ? labelName : `&${labelName}`;
   }
 }
