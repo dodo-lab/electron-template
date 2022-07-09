4b98cfe112a41fe5139f8670a34a007d238f3bad
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-07-10 07:55:29 +0900

    💡: chore

--- src/common/ipc.ts
+++ src/common/ipc.ts
@@ -1,13 +1,25 @@
+/**
+ * メインプロセスからレンダープロセスへの通信.
+ * @note 戻り値はvoid型にすること.
+ */
 export type IpcMainToRender = {
   sample: (text: string, count: number) => void;
 };
 export type IpcMainToRenderKey = keyof IpcMainToRender;
 
+/**
+ * レンダープロセスからメインプロセスへの通信.
+ * @note 戻り値はvoid型にすること.
+ */
 export type IpcRenderToMain = {
   sample: (count: number, text: string) => void;
 };
 export type IpcRenderToMainKey = keyof IpcRenderToMain;
 
+/**
+ * レンダープロセスからメインプロセスへの双方向通信.
+ * @note 戻り値はPromise型にすること.
+ */
 export type IpcRenderToMainInvoke = {
   sample: (flag: boolean, count: number) => Promise<string>;
 };
