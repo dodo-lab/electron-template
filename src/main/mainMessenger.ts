ac5fd3b6077e399f60392cb1ea8c0650aa21e2cd
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-07-10 09:34:26 +0900

    🐛: deprecateとなったeventを使わない

--- src/main/mainMessenger.ts
+++ src/main/mainMessenger.ts
@@ -19,29 +19,30 @@ export abstract class MainMessenger {
     this.mainWindow?.webContents.send(key, ...args);
   }
 
-  static on<T extends IpcRenderToMainKey>(
-    key: T,
-    callback: (event: IpcMainEvent, args: Parameters<IpcRenderToMain[T]>) => void,
-  ) {
-    ipcMain.on(key, callback);
-    return () => ipcMain.removeListener(key, callback);
+  static on<T extends IpcRenderToMainKey>(key: T, callback: (args: Parameters<IpcRenderToMain[T]>) => void) {
+    const listener = (_: IpcMainEvent, args: Parameters<IpcRenderToMain[T]>) => {
+      callback(args);
+    };
+    ipcMain.on(key, listener);
+    return () => ipcMain.removeListener(key, listener);
   }
 
-  static once<T extends IpcRenderToMainKey>(
-    key: T,
-    callback: (event: IpcMainEvent, args: Parameters<IpcRenderToMain[T]>) => void,
-  ) {
-    ipcMain.once(key, callback);
+  static once<T extends IpcRenderToMainKey>(key: T, callback: (args: Parameters<IpcRenderToMain[T]>) => void) {
+    const listener = (_: IpcMainEvent, args: Parameters<IpcRenderToMain[T]>) => {
+      callback(args);
+    };
+    ipcMain.once(key, listener);
   }
 
   static handle<T extends IpcRenderToMainInvokeKey>(
     key: T,
-    callback: (
-      event: IpcMainInvokeEvent,
-      ...args: Parameters<IpcRenderToMainInvoke[T]>
-    ) => ReturnType<IpcRenderToMainInvoke[T]>,
+    callback: (...args: Parameters<IpcRenderToMainInvoke[T]>) => ReturnType<IpcRenderToMainInvoke[T]>,
   ) {
+    const listener = (_: IpcMainInvokeEvent, ...args: Parameters<IpcRenderToMainInvoke[T]>) => {
+      return callback(...args);
+    };
+
     // @ts-ignore
-    ipcMain.handle(key, callback);
+    ipcMain.handle(key, listener);
   }
 }
