2268769ce28ccdd67edfd86d7ca4075628154e66
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-07-10 11:19:41 +0900

    ✨: 初期ページをmuiベースに

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,42 +1,23 @@
+import {CssBaseline, Stack, Typography} from '@mui/material';
 import {MemoryRouter as Router, Route, Routes} from 'react-router-dom';
-import icon from '../../assets/icon.svg';
-import './App.css';
 
-const Hello = () => {
+const Sample = () => {
   return (
-    <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a href="https://electron-react-boilerplate.js.org/" target="_blank" rel="noreferrer">
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a href="https://github.com/sponsors/electron-react-boilerplate" target="_blank" rel="noreferrer">
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
+    <Stack sx={{justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: 'lightblue'}}>
+      <Typography variant="h2">Electron template</Typography>
+    </Stack>
   );
 };
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Hello />} />
-      </Routes>
-    </Router>
+    <>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Sample />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
